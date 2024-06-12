9707b539e057f68bc7c2609e771211c8cdd84d4f
LorraineAdebowale <lorrainet85@gmail.com>
2024-06-12 23:39:45 +0100

    solving deployment issues

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,9 @@
-/// <reference types="astro/client" />
-declare namespace App {
-  interface Locals {
-    email: string;
-  }
+
+interface ImportMetaEnv {
+  readonly SUPABASE_URL: string
+  readonly SUPABASE_ANON_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
